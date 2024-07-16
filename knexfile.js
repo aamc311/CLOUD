3d520912372455bb87d8f20c478a4de6bd0bc258
Alison <alison.marcalla2692@utc.edu.ec>
2024-07-16 08:56:50 -1200

    hola

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host:'127.0.0.1',
+      host:'localhost',
       user:'root',
       password:'',
       database:'cloudAli'
